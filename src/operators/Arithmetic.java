54524fc3594cf2f929a1ad26eae5b90ddcb6e255
Sudha Chinnappa <sudha.chinnappa@gmail.com>
2021-02-04 09:33:48 -0800

    Update Arithmetic.java

--- src/operators/Arithmetic.java
+++ src/operators/Arithmetic.java
@@ -9,9 +9,38 @@ public class Arithmetic {
     }
 
     public  int subtraction(int number1, int number2) {
-        int subtraction;
-        subtraction = number1 - number2;
-        return subtraction;
+        int difference;
+        difference = number1 - number2;
+        return difference;
     }
 
+    public  int multiplication(int number1, int number2) {
+        int product;
+        product = number1 * number2;
+        return product;
+    }
+    
+    public  int division(int dividend, int divisor) {
+        int quotient;
+        quotient = dividend / divisor;
+        return quotient;
+    }
+    
+    public  int modulus(int dividend, int divisor) {
+        int remainder;
+        remainder = dividend % divisor;
+        return remainder;
+    }
+    
+    public  int increment(int number) {
+        int incrementByOne;
+        incrementByOne = number++;  // This is the short form of: incrementByOne = number + 1;
+        return incrementByOne;
+    }
+    
+    public  int decrement(int number) {
+        int decrementByOne;
+        decrementByOne = number--;  // This is the short form of: incrementByOne = number -1 1;
+        return decrementByOne;
+    }
 }
