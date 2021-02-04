dcbe67a9d8c25f232f612a3088b6481188d6534a
Sudha Chinnappa <sudha.chinnappa@gmail.com>
2021-02-04 12:05:42 -0800

    Update main.java

--- src/main.java
+++ src/main.java
@@ -3,10 +3,13 @@ import operators.Relational;
 
 public class main {
 
+    // Main method: When this project is run, the starting point of execution is this method
     public static void main(String[] args) {
 
+        // Create an abject of type Arithmetic
         Arithmetic math = new Arithmetic();
 
+        // Call methods in Arithmetic class using "math" object
         int s = math.add(1, 2);
         System.out.println("Sum of 1 and 2: " + s);
 
@@ -20,22 +23,26 @@ public class main {
         System.out.println("Decrement 4 by 1: " + math.decrement(4));
         // ...
         // ...
-            
+        
+        // Create an abject of type Relational
         Relational relation = new Relational();
         
-        System.out.println("is 4 equals 3: " + relation.isGreaterThan(4, 3));
-        System.out.println("is 4 greater than 3: " + relation.isGreaterThan(4, 3));
-        System.out.println("is 4 less than 3: " + relation.isLessThan(4, 3));
+        // Call methods in Relational class using "relation" object
+        System.out.println("Is 4 equals 3: " + relation.isGreaterThan(4, 3));
+        System.out.println("Is 4 greater than 3: " + relation.isGreaterThan(4, 3));
+        System.out.println("Is 4 less than 3: " + relation.isLessThan(4, 3));
         
         // ...
         // ...
         
+        // Create an abject of type Bitwise
         Bitwise bitwise = new Bitwise();
         
+        // Call methods in Bitwise class using "bitwise" object
         System.out.println("Bitwise And on 4 and 3: " + bitwise.and(4, 3));
         System.out.println("Bitwise Or on 4 and 3: " + bitwise.or(4, 3));
         System.out.println("Bitwise Exclusive OR on 4 and 3: " + bitwise.exclusiveOr(4, 3));
-        System.out.println("Bitwise complement of 4: " + bitwise.complement(4, 3));
+        System.out.println("Bitwise Complement of 4: " + bitwise.complement(4));
         
         // ...
         // ...
